e64b6b600a4ce582799c6ad3b7c2a37740342b76
nryas <nryas@me.com>
2016-03-08 10:03:26 +0900

    add autoload_path

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Meali
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths += %w(#{config.root}/lib)
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
