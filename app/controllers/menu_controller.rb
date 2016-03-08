22035d3aa441c8791314ab6a9614c20899e3a131
nryas <nryas@me.com>
2016-03-08 17:37:31 +0900

    clean MenuController

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,25 +1,21 @@
 class MenuController < ApplicationController
-
   @@StoreDir = "tmp/menu"
 
   def create
     file = params[:attachment]
-
     if file.present?
-      Dir.mkdir( @@StoreDir ) unless Dir.exists?(@@StoreDir)
+      Dir.mkdir(@@StoreDir) unless Dir.exists?(@@StoreDir)
       FileUtils.mv( file.path, File.join(@@StoreDir, file.original_filename))
 
       begin
-        MenuUtils.execute( File.join(@@StoreDir, file.original_filename) )
-        redirect_to "/admin/dashboard", notice: "成功"
+        MenuUtils.import(File.join(@@StoreDir, file.original_filename))
+        redirect_to :back, notice: "成功"
       rescue => e
-        redirect_to "/admin/dashboard", alert: "エラー:#{e.message}"
+        redirect_to :back, alert: "エラー:#{e.message}"
       end
 
     else
-      redirect_to "/admin/dashboard", alert: "献立ファイルを選択してください"
+      redirect_to :back, alert: "献立ファイルを選択してください"
     end
-
   end
-
 end
