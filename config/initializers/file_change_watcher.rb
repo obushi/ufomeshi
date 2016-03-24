4a1f73bf71b4f2ee022ea8a2127938abe35f5ac7
nryas <nryas@me.com>
2016-03-24 19:25:13 +0900

    resolve test error

--- config/initializers/file_change_watcher.rb
+++ config/initializers/file_change_watcher.rb
@@ -1,3 +1,5 @@
+require "listen"
+
 listener = Listen.to(Meali::Application.config.menu_root) do |modified, added, removed|
   Meal.convert(added.first) if File.extname(added.first.to_s) == ".xlsx"
 end
