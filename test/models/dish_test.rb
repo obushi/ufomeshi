5c340c8d18462ec2bb0c3749a721898cd1dae13d
nryas <nryas@me.com>
2016-03-27 23:47:26 +0900

    add test codes for models

--- test/models/dish_test.rb
+++ test/models/dish_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class DishTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save dish" do
+    dish = Dish.new(name: "献立", calorie: 120)
+    assert dish.save
+  end
 end
--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MealTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save meal" do
+    params = {
+      served_on: Date.today,
+      meal_type: 0,
+      calorie: 100,
+      protein: 50,
+      fat: 10,
+      carbohydrate: 20,
+      salt: 3.5,
+      dishes_attributes: [{name: "食べ物", calorie: 200}, {name: "デザート", calorie: 100}]
+    }
+    meal = Meal.new(params)
+    assert meal.save
+  end
 end
