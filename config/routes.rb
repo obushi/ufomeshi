616dfb8233c35b1ca70094c213098975ac7a88f2
nryas <nryas@me.com>
2016-03-29 13:42:06 +0900

    add meal's views

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :meals, only: [:index, :show, :create] do
+  resources :meals, only: [:index, :show] do
     collection do
       post 'upload'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
