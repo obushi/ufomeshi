0c2aa5b4335187f02b3f4955e862cd5af1b0f224
nryas <nryas@me.com>
2016-04-30 16:31:16 +0900

    modify routing constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
+date_constraints = lambda { |req|
+  return true if req.params[:date].blank?
+  Date.valid_date?(
+    req.params[:date][0..3].to_i,
+    req.params[:date][4..5].to_i,
+    req.params[:date][6..7].to_i)
+  }
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root 'meals#show'
 
-  resources :meals, only: [:index, :show], param: :date, constraints: lambda{ |req| Date.valid_date?(req.params[:date][0..3].to_i, req.params[:date][4..5].to_i, req.params[:date][6..7].to_i) } do
+  resources :meals, only: [:index, :show], param: :date, constraints: date_constraints do
     collection do
       post 'upload'
     end
@@ -12,8 +20,11 @@ Rails.application.routes.draw do
 
   namespace :api, { format: 'json' } do
     namespace :v1 do
-      resources :meals, only: [:index, :show], param: :date, constraints: lambda{ |req| Date.valid_date?(req.params[:date][0..3].to_i, req.params[:date][4..5].to_i, req.params[:date][6..7].to_i) }
-      get 'search' => 'meals#search'
+      resources :meals, only: [:index, :show], param: :date, constraints: date_constraints do
+        collection do
+          get 'search'
+        end
+      end
     end
   end
   # match '*path' => 'application#error404', via: :all
