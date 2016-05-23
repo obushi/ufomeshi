55102b110a6177b6a2009c5e3b573159ca55caef
nryas <nryas@me.com>
2016-05-24 08:11:53 +0900

    add more schedule of twitter bot

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,15 @@
-every '0 8 * * *' do
+every '0 5 * * *' do
+  rake 'tweet:all'
+end
+
+every '0 7 * * *' do
   rake 'tweet:breakfast'
+end
+
+every '30 11 * * *' do
+  rake 'tweet:lunch'
+end
+
+every '30 17 * * *' do
+  rake 'tweet:dinner'
 end
